175b2a3ede1423ab9f4888e42d81674650dd6ed6
Eli Dowling <eli.jambu@yahoo.com>
2019-01-15 12:21:16 +1000

    finished elk spawning

--- Assets/Scripts/ElkSpawner.cs
+++ Assets/Scripts/ElkSpawner.cs
@@ -9,16 +9,19 @@ public class ElkSpawner : MonoBehaviour
     public int maxGroupSize = 20;
     public int minGroupSize = 5;
     public GameObject elkPrefab;
+    public GameObject groupManagerObject;
     // Use this for initialization
     void Start()
     {
+        GroupmanagerComponent groupManager = groupManagerObject.GetComponent<GroupmanagerComponent>();
         for (int i = 0; i < numberOfGroups; i++)
         {
             int groupSize = Random.Range(minGroupSize, maxGroupSize); //This should really follow a nroaml distribution but for now uitll just be random
-            CreateGroup(groupSize, new Vector3(0, 0, i * 20), i);
+            CreateGroup(groupSize, new Vector3(0, 0, i * 20), i, groupManager);
         }
+
     }
-    void CreateGroup(int size, Vector3 location, int groupID)
+    void CreateGroup(int size, Vector3 location, int groupID, GroupmanagerComponent groupManager)
     {
         int columnLength = Mathf.CeilToInt(Mathf.Sqrt(size));
         int spawned = 0;
@@ -29,10 +32,21 @@ public class ElkSpawner : MonoBehaviour
             {
                 if (spawned >= size) break;
                 GameObject elk = Instantiate(elkPrefab, startPoint + new Vector3(i, 0, j), Quaternion.identity);
-                elk.GetComponent<GroupComponent>().ID = groupID;
+                GroupComponent groupComponent = elk.GetComponent<GroupComponent>();
+                groupComponent.ID = groupID;
+
+                //This monstrosity creates fills the groupmanagers array containing all the prey for each group
+                groupManager.PreyTransform[groupID][groupManager.PreyTransform[groupID].Length - 1] = elk.transform;
             }
             if (spawned >= size) break;
         }
-    }
 
+    }
+    void GetPreyForEachGroup(GroupmanagerComponent groupManager)
+    {
+        for (int i = 0; i < .Length; i++)
+        {
+
+        }
+    }
 }

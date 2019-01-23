612534dfd26c3fcb882e80f4bd6ae82c7a495b07
faldor20 <eli.jambu@gmail.com>
2019-01-23 10:38:01 +1000

    added ecs boids example

---
+++ Assets/ECS BOIDs/Scripts/HeadingSystem.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Samples.Common
+{
+    //This system is somewhat poorly named.
+    // It actually sets the rotation from the heading.
+    //the heading is set in a nother system
+    // why does this system need to exist? could the rotation not have been set directly?
+    public class HeadingSystem : JobComponentSystem
+    {
+        [BurstCompile]
+        struct RotationFromHeading : IJobProcessComponentData<Heading, Rotation>
+        {
+            public void Execute([ReadOnly] ref Heading heading, ref Rotation rotation)
+            {
+                var rotationFromHeading = quaternion.LookRotationSafe(heading.Value, math.up());
+                rotation = new Rotation { Value = rotationFromHeading };
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var rotationFromHeadingJob = new RotationFromHeading();
+            var rotationFromHeadingJobHandle = rotationFromHeadingJob.Schedule(this, inputDeps);
+
+            return rotationFromHeadingJobHandle;
+        }
+    }
+}
---
+++ Assets/ECS BOIDs/Scripts/MyECSBoids/MainBoidComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+public struct MainBoid : ISharedComponentData
+{
+    public float seperationMaxDistance;
+    public float separationWeight;
+    public float seperationrange;
+    public bool scaleSeperationWeight;
+
+    public float alignmentMaxDistance;
+    public float alignmentWeight;
+    public float alignmentRange;
+    public bool scaleAlignmentWeight;
+
+    public float targetMaxDistance;
+    public float targetWeight;
+    public bool scaleTargetWeight;
+
+    public float predatorFleeDistance;
+    public float predatorFleeMaxDistance;
+
+    public float group;
+
+    public float3 heading;
+}
+
+public class MainBoidComponent : SharedComponentDataWrapper<MainBoid> { }
